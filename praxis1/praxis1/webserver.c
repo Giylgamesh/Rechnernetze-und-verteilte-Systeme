9cd7a6c0c5d71a66ac8a69ceb30c1e6b93155fff
Tino Liang <tino.liang@campus.tu-berlin.de>
2024-12-07 20:50:07 +0100

    added package recog and buffer in recv loop

--- praxis1/praxis1/webserver.c
+++ praxis1/praxis1/webserver.c
@@ -9,14 +9,23 @@
 #include <netinet/in.h>
 
 #define BACKLOG 10     // Maximale Anzahl an Connections in der Listen-Queue
-#define MAXDATASIZE 100
+#define MAXDATASIZE 1024
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 int main(int argc, char *argv[])
 {
     char buf[MAXDATASIZE];
-    struct sockaddr_storage their_addr;
+    struct sockaddr_storage client_addr;
     int status;
-    int sockfd, new_fd;
+    int sock_fd, client_fd;
     socklen_t addr_size;
     struct addrinfo hints, *servinfo;
     int rv;
@@ -40,17 +49,21 @@ int main(int argc, char *argv[])
 
 
     // Socket initialisieren
-    sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-    printf("\nSocket() File Descriptor value: %d\n", sockfd);
+    if ((sock_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1) {
+        fprintf(stderr, "socket() init error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    printf("\nSocket() initialized File Descriptor value: %d\n", sock_fd);
 
 
     // Socket-Discriptor an die Adresse Binden
-    if ((status = bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen)) == -1) {
+    if ((status = bind(sock_fd, servinfo->ai_addr, servinfo->ai_addrlen)) == -1) {
         fprintf(stderr, "bind error: %s\n", gai_strerror(status));
     }
 
     int yes=1;
-    if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1) {
+    if (setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1) {
         perror("setsockopt failed...");
         exit(1);
     }
@@ -58,37 +71,86 @@ int main(int argc, char *argv[])
 
     // Auf Connections Horchen/Listen
     printf("\nOpening Listen Socket...\n\n");
-    listen(sockfd, BACKLOG);
-
+    listen(sock_fd, BACKLOG);
 
     // Neue Verbindungen aus der Listen-Queue akzeptieren
-    addr_size = sizeof their_addr;
-    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
-    if (new_fd == -1) {
-        perror("Couldn't accept any Connection!");
+    addr_size = sizeof client_addr;
+    client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &addr_size);
+    if (client_fd == -1) {
+        perror("Couldn't accept Connection!");
     }
 
+    inet_ntop(client_addr.ss_family,
+              get_in_addr((struct sockaddr *)&client_addr),
+              s, sizeof s);
+    printf("Server accepted connection from %s\n\n", s);
 
     int numbytes;
-    if ((numbytes = recv(new_fd, buf, MAXDATASIZE-1, 0)) == -1) {
-        perror("recv failed...");
-        exit(1);
+    int buf_index = 0;
+    char recv_buf[MAXDATASIZE];
+    memset(recv_buf, 0, MAXDATASIZE);
+    while(1)  {
+        if ((numbytes = recv(client_fd, buf, MAXDATASIZE-1, 0)) == -1) {
+            perror("recv failed...");
+            exit(1);
+        }
+
+        if (numbytes == 0) {
+            printf("No Bytes received, closing Server...");
+            break;
+        }
+
+        buf[numbytes] = '\0';
+        printf("Client Request: '%s'\n\n",buf);
+
+        /*
+         * Der main Buffer buf wird an den temporären Receive Buffer recv_buf angehängt
+         * Die stelle an der die Angehängt wird entspricht index += numbytes durch jede Iteration,
+         * wo das Paket noch nicht beendet wurde.
+         *
+         **/
+        memcpy(recv_buf + buf_index, buf, numbytes);
+        buf_index += numbytes;
+
+        /*
+         * Nullterminator muss, NACHDEM bufindex erhöht wird, gesetzt werden und nicht davor!
+         * Hier wird außerdem noch der main Buffer buf "gecleared", also mit 0 intitialisiert.
+         *
+         **/
+        recv_buf[buf_index] = '\0';
+        memset(&buf, 0, numbytes);
+
+        printf("buf after memset: '%s'\n\n",buf);
+        printf("BUFFER READ: '%s'\n\n", recv_buf);
+
+        // Überprüft den Byte-Strom nach der Paket-Endung
+        char *recv_end = strstr(recv_buf, "\r\n\r\n");
+
+        /*
+         * Bei erfolgreichem finden eines Paketendes wird eine Antwort versendet
+         **/
+        if (recv_end != NULL) {
+            printf("Recieved Package with ending... \n");
+            // Antwort bei Erfolgreicher Verbindung zum Socket und korrekter Paket-Endung
+            char *msg = "Reply\r\n\r\n";
+            int len, bytes_sent;
+            len = strlen(msg);
+            bytes_sent = send(client_fd, msg, len, 0);
+
+            /*
+             * move_index errechnet sich aus dem buf_index also der länge des Buffers minus
+             * der verbleibenden Länge des Buffers nachdem ein Paket erkannt wurde, also
+             * nach \r\n\r\n.
+             */
+            int move_index = buf_index - (recv_end + 4 - recv_buf);
+            memmove(recv_buf, recv_buf + (recv_end + 4 - recv_buf), move_index);
+            buf_index = 0;
+            printf("Bytes send: %d\n\n", bytes_sent);
+        }
     }
 
-    buf[numbytes] = '\0';
-    printf("Client: '%s'\n",buf);
-
-
-    // Antwort bei Erfolgreicher Verbindung zum Socket
-    char *msg = "Reply";
-    int len, bytes_sent;
-    len = strlen(msg);
-    bytes_sent = send(new_fd, msg, len, 0);
-
-    printf("Bytes send: %d", bytes_sent);
-    close(sockfd);
-    close(new_fd);
-
+    close(client_fd);
+    close(sock_fd);
     freeaddrinfo(servinfo);
     
     return 0;
