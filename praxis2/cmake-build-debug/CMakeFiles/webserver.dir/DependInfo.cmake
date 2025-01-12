f9e88d0f0355d39d4ed5afafe51766fe5658d9c5
TinoLiang <tino.liang@campus.tu-berlin.de>
2025-01-12 20:50:07 +0100

    added udp socket, resource hashing, response and dht responsibility check and more...

---
+++ praxis2/cmake-build-debug/CMakeFiles/webserver.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/d/RNVS/Rechnernetze-und-verteilte-Systeme/praxis2/data.c" "/mnt/d/RNVS/Rechnernetze-und-verteilte-Systeme/praxis2/cmake-build-debug/CMakeFiles/webserver.dir/data.c.o"
+  "/mnt/d/RNVS/Rechnernetze-und-verteilte-Systeme/praxis2/http.c" "/mnt/d/RNVS/Rechnernetze-und-verteilte-Systeme/praxis2/cmake-build-debug/CMakeFiles/webserver.dir/http.c.o"
+  "/mnt/d/RNVS/Rechnernetze-und-verteilte-Systeme/praxis2/util.c" "/mnt/d/RNVS/Rechnernetze-und-verteilte-Systeme/praxis2/cmake-build-debug/CMakeFiles/webserver.dir/util.c.o"
+  "/mnt/d/RNVS/Rechnernetze-und-verteilte-Systeme/praxis2/webserver.c" "/mnt/d/RNVS/Rechnernetze-und-verteilte-Systeme/praxis2/cmake-build-debug/CMakeFiles/webserver.dir/webserver.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
